91a4bdee77b07a78107a7e29b2aedc65977d7c84
Nikolazard <nikolas.tutrut@gmail.com>
2025-04-27 15:39:06 -0300

    1.0

---
+++ script/c3000000002.lua
@@ -0,0 +1,9 @@
+--Mini Plinio
+function c3000000002.initial_effect(c)
+	-- ataque duplo
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EXTRA_ATTACK)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+end
